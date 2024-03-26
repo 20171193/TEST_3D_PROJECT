4db0bb723d8556567b2f92c077227a603df2f8ea
Shim Jae Chun <shimnaldo98@gmail.com>
2024-03-26 19:56:35 +0900

    issue : player digger trigger

---
+++ Assets/01. Scripts/Craft/Object/DiggableObject.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiggableObject : MonoBehaviour
+{
+    [SerializeField]
+    private Material diggableMT;
+
+    private MeshRenderer meshRenderer;
+    private Material[] originMT;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        originMT = meshRenderer.materials;
+    }
+
+    public void OnTargeted()
+    {
+        meshRenderer.material = diggableMT;
+    }
+    public void OffTargeted()
+    {
+        meshRenderer.materials = originMT;
+    }
+}
---
+++ Assets/01. Scripts/Craft/Player/PlayerDigger.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDigger : MonoBehaviour
+{
+    // 캘 수 있는 모든 오브젝트에 적용할 머터리얼
+    [SerializeField]
+    private Material diggableMT;
+
+    [SerializeField]
+    private GameObject targetObject;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name);
+        if(other.gameObject.layer == LayerMask.NameToLayer("Diggable"))
+        {
+            targetObject = other.gameObject;
+            targetObject.GetComponent<DiggableObject>().OnTargeted();
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == targetObject)
+        {
+            targetObject.GetComponent<DiggableObject>().OffTargeted();
+            targetObject = null;
+        }
+    }
+
+}
